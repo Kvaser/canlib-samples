9e8eb184ff7df30286af2174b7dcf02735e02e29
Eliot Blennerhassett <eliot@blennerhassett.gen.nz>
2015-09-02 10:28:39 +1200

    Convert read, write, writeWait to use str (or bytes)
    Reading or writing from 'str' is similar to how read/write methods
    work for e.g. file, Serial etc.
    
    This is useful because str is compatible with struct.pack/unpack

--- Samples/Python/canlib.py
+++ Samples/Python/canlib.py
@@ -609,27 +609,35 @@ class canChannel(object):
     # The variable name id (as used by canlib) is a built-in function in Python,
     # so we use the name id_ instead
     def write(self, id_, msg, flag=0):
-        self.canlib.fn = inspect.currentframe().f_code.co_name
-        data = (c_ubyte * len(msg))(*msg)
-        self.dll.canWrite(self.handle, id_, byref(data), len(msg), flag)
+        self.canlib.fn = 'write'
+        self.dll.canWrite(self.handle, id_, msg, len(msg), flag)
 
     def writeWait(self, id_, msg, flag=0, timeout=0):
         self.canlib.fn = inspect.currentframe().f_code.co_name
-        data = (c_ubyte * len(msg))(*msg)
-        self.dll.canWriteWait(self.handle, id_, byref(data), len(msg), flag,
-                              timeout)
+        self.dll.canWriteWait(self.handle, id_, msg, len(msg), flag, timeout)
 
     def read(self, timeout=0):
-        self.canlib.fn = inspect.currentframe().f_code.co_name
-        msg = self.canlib.canMessage()
+        """Read a CAN message and metadata.
+
+        :param timeout: Timeout in seconds, default 0
+
+        :returns (
+            CAN ID, 
+            Message_data - max length 8,
+            DLC - normally same as message length, but may be > 8?,
+            Flags - a combination of the canMSG_xxx and canMSGERR_xxx values,
+            Time - timestamp from hardware
+        )
+        :rtype (int, bytes, int, int, float)
+        """
+        self.canlib.fn = 'read'
+        msg = create_string_buffer(8)
         id_ = c_long()
         dlc = c_uint()
         flag = c_uint()
         time = c_ulong()
-        self.dll.canReadWait(self.handle, byref(id_), byref(msg), byref(dlc),
-                             byref(flag), byref(time), timeout)
-        msgList = [msg[i] for i in range(len(msg))]
-        return id_.value, msgList[:dlc.value], dlc.value, flag.value, time.value
+        self.dll.canReadWait(self.handle, id_, msg, dlc, flag, time, timeout)
+        return id_.value, msg[:dlc.value], dlc.value, flag.value, time.value
 
     def readDeviceCustomerData(self, userNumber=100, itemNumber=0):
         self.fn = inspect.currentframe().f_code.co_name
